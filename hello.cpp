ef1929101223c646c3b48d489c44e5ccaacfaf9a
chandanicodes <chandanis534@gmail.com>
2022-09-01 15:53:09 +0530

    uploading

---
+++ hello.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <cctype>
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int size;
+    cin >> size;
+    int arr[size];
+
+    for (int i = 0; i < size; i++)
+    {
+        cin >> arr[i];
+    }
+
+    // sort(arr, arr + size);
+    // cout << arr[size - 2];
+
+    for(int i=0 ; i< size; i++){
+        for(int j=i;)
+    }
+    return 0;
+}
